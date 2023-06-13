5706eea244296514f66dbc8bc09ecdb931a473a5
Astrid Serrano <yari12_94@hotmail.com>
2023-06-13 13:18:00 -0600

    toDo.js

---
+++ toDo.js
@@ -0,0 +1,12 @@
+const contenedorTareas = document.querySelector("#tareaInput");
+const boton = document.querySelector("#btn-agregar");
+
+boton.addEventListener("click", function () {
+  let tarea = contenedorTareas.value;
+
+  let texto = document.createElement("p");
+
+  texto.innerHTML = tarea;
+
+  document.getElementById("listaTarea").appendChild(texto);
+});
