2fe12e5c21f473ace8e0cb19ade289516939539c
Yariserrano12 <yari12_94@hotmail.com>
2023-06-13 22:50:45 -0600

    avance martes noche

--- toDo.js
+++ toDo.js
@@ -1,7 +1,9 @@
 const contenedorTareas = document.querySelector("#tareaInput");
 const boton = document.querySelector("#btn-agregar");
+var ul= document.querySelector("ul");
+var empty= document.querySelector("empty");
 
-boton.addEventListener("click", function () {
+function agregarTarea() {
   let tarea = contenedorTareas.value;
 
   let texto = document.createElement("p");
@@ -9,10 +11,47 @@ boton.addEventListener("click", function () {
   texto.innerHTML = tarea;
 
   document.getElementById("listaTarea").appendChild(texto);
-});
+
+  contenedorTareas.value = "";
+};
+
+
+boton.addEventListener("click",agregarTarea);
+
+contenedorTareas.addEventListener("keypress", function(event) { 
+  if (event.key === "Enter") {
+    event.preventDefault();
+    agregarTarea();
+  } 
+
+
+}) 
+const li= document.createElement("li") 
+const p= document.createElement("p") 
+p.textContent 
+li.appendChild(p) 
+ul.appendChild("li") 
+li.appendChild(addDeleteBtn());
+
+function addDeleteBtn( ) { 
+  const deleteBtn= document.createElement("button")  
+  deleteBtn.textContent = "X";
+  deleteBtn.className= "btn-delete"; 
+
+  deleteBtn.addEventListener("click", (e) => {
+    const item = e.target.parentElement;
+    ul.removeChild(item);
+
+  } ) 
+  return deleteBtn
+  
+}
 
 
 
 
 
-const enter= listaTarea 
+
+
+
+
