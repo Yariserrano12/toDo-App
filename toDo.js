1f4f391d867a67894cff8ea51152e601b4d69c6e
Astrid Serrano <yari12_94@hotmail.com>
2023-06-14 16:08:45 -0600

    avance miercoles js

--- toDo.js
+++ toDo.js
@@ -1,19 +1,43 @@
 const contenedorTareas = document.querySelector("#tareaInput");
 const boton = document.querySelector("#btn-agregar");
-var ul= document.querySelector("ul");
-var empty= document.querySelector("empty");
+var listaTarea= document.querySelector("listaTarea");
+
 
 function agregarTarea() {
+  
   let tarea = contenedorTareas.value;
 
   let texto = document.createElement("p");
+  texto.id = "arreglartexto"
+  let inputC= document.createElement("input") 
+  inputC.id = "arreglar check"
+  inputC.setAttribute("type", "checkbox") 
+  
+  let eliminar = document.createElement("button") 
+  eliminar.id = "arreglar boton"
+
+  eliminar.textContent="Eliminar";
+
+ console.log (eliminar)
+
+
 
   texto.innerHTML = tarea;
+  
 
-  document.getElementById("listaTarea").appendChild(texto);
+  
 
-  contenedorTareas.value = "";
-};
+let tabla= document.getElementById("listaTarea")
+tabla.appendChild(texto)
+tabla.appendChild(inputC)
+tabla.appendChild(eliminar)
+
+
+
+  contenedorTareas.value = ""; 
+
+
+}; 
 
 
 boton.addEventListener("click",agregarTarea);
@@ -22,30 +46,22 @@ contenedorTareas.addEventListener("keypress", function(event) {
   if (event.key === "Enter") {
     event.preventDefault();
     agregarTarea();
-  } 
+  }  
+
 
 
 }) 
-const li= document.createElement("li") 
-const p= document.createElement("p") 
-p.textContent 
-li.appendChild(p) 
-ul.appendChild("li") 
-li.appendChild(addDeleteBtn());
 
-function addDeleteBtn( ) { 
-  const deleteBtn= document.createElement("button")  
-  deleteBtn.textContent = "X";
-  deleteBtn.className= "btn-delete"; 
 
-  deleteBtn.addEventListener("click", (e) => {
-    const item = e.target.parentElement;
-    ul.removeChild(item);
 
-  } ) 
-  return deleteBtn
-  
-}
+
+
+
+
+
+
+
+
 
 
 
