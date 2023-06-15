93c5fbf6386bc280e3478ea3d4fad966c8e1dc45
Astrid Serrano <yari12_94@hotmail.com>
2023-06-15 16:07:05 -0600

    crear funcion eliminar

--- toDo.js
+++ toDo.js
@@ -1,78 +1,59 @@
 const contenedorTareas = document.querySelector("#tareaInput");
 const boton = document.querySelector("#btn-agregar");
-var listaTarea = document.querySelector("listaTarea");
-var letra = document.querySelector("#letra");
-var check = document.querySelector("#check");
-let contadorT=0; 
-var mensaje = document.querySelector("#mensaje") 
-
-
+let listaTarea = document.querySelector("#listaTarea");
+let letra = document.querySelector("#letra");
+let check = document.querySelector("#check");
+let contadorT = 0;
+let mensaje = document.querySelector("#mensaje");
 
 function agregarTarea() {
-
-
   let tarea = contenedorTareas.value;
 
+  let tareaLi = document.createElement("li");
+
+  let texto = document.createElement("p");
+  texto.id = "arreglartexto";
+
+  let inputC = document.createElement("input");
+  inputC.id = "arreglarcheck";
+  inputC.setAttribute("type", "checkbox");
+
+  let eliminar = document.createElement("button");
+  eliminar.id = "arreglarboton";
+  eliminar.textContent = "Eliminar";
+
+  texto.innerHTML = tarea;
+  tareaLi.appendChild(texto);
+  tareaLi.appendChild(inputC);
+  tareaLi.appendChild(eliminar);
+
+  let tabla = document.getElementById("listaTarea");
+  tabla.appendChild(tareaLi);
+
+  eliminar.addEventListener("click", (e) => {
+    let listaT = document.getElementById("listaTarea");
+    const i = e.target.parentElement;
+    listaT.removeChild(i);
+  });
+
+  contenedorTareas.value = "";
+
+  inputC.addEventListener("click", function () {
+    if (inputC.checked) {
+      contadorT = contadorT + 1;
+
+      letra.innerHTML = contadorT;
+    } else {
+      contadorT = contadorT - 1;
+      letra.innerHTML = contadorT;
+    }
+  });
+
   if (!tarea == "") {
-    
-    let texto = document.createElement("p");
-    texto.id = "arreglartexto"
-    
-    let inputC = document.createElement("input")
-    inputC.id = "arreglarcheck"
-    inputC.setAttribute("type", "checkbox")
-  
-    let eliminar = document.createElement("button")
-    eliminar.id = "arreglarboton"
-    eliminar.textContent = "Eliminar";
-  
-  
-    texto.innerHTML = tarea;
-    let tabla = document.getElementById("listaTarea")
-    tabla.appendChild(texto)
-    tabla.appendChild(inputC)
-    tabla.appendChild(eliminar) 
-
-    eliminar.addEventListener("click", function() {
-      if (listaTarea) {
-        listaTarea.removeChild(listaTarea)
-      }
-    }) 
-
-    
-
-    contenedorTareas.value = "";
-
-    inputC.addEventListener("click",function () {
-      if (inputC.checked) {
-       
-       contadorT=contadorT+1
-     
-       letra.innerHTML=contadorT
-    
-
-      }else{
-        
-       contadorT=contadorT-1; 
-          letra.innerHTML=contadorT
-        
-      }
-      
-    })
-    
-  
- 
-
-  }else{
+  } else {
     alert("Ingrese una tarea válida");
   }
-
-     
-
-
 }
-
-
 
 boton.addEventListener("click", agregarTarea);
 
@@ -81,28 +62,4 @@ contenedorTareas.addEventListener("keypress", function (event) {
     event.preventDefault();
     agregarTarea();
   }
-
-}) 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
