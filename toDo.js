a6a6077de1167ae8c108deadc02e990a4e9c5cef
Astrid Serrano <yari12_94@hotmail.com>
2023-06-16 11:48:37 -0600

    agregar alerta

--- toDo.js
+++ toDo.js
@@ -4,11 +4,13 @@ let listaTarea = document.querySelector("#listaTarea");
 let letra = document.querySelector("#letra");
 let check = document.querySelector("#check");
 let contadorT = 0;
-let mensaje = document.querySelector("#mensaje");
+let mensaje = document.getElementById("mensaje");
 
-function agregarTarea() {
+
+function agregarTarea() { 
+  
   let tarea = contenedorTareas.value;
-
+  if (tarea !== "") {
   let tareaLi = document.createElement("li");
 
   let texto = document.createElement("p");
@@ -20,8 +22,8 @@ function agregarTarea() {
 
   let eliminar = document.createElement("button");
   eliminar.id = "arreglarboton";
-  eliminar.textContent = "Eliminar"; 
-
+  eliminar.textContent = "Eliminar";
+ 
   texto.innerHTML = tarea;
   tareaLi.appendChild(texto);
   tareaLi.appendChild(inputC);
@@ -30,32 +32,57 @@ function agregarTarea() {
   let tabla = document.getElementById("listaTarea");
   tabla.appendChild(tareaLi);
 
+ mensaje.style.display="none"
+
+
   eliminar.addEventListener("click", (e) => {
     let listaT = document.getElementById("listaTarea");
+     
+
+
+
+
+
     const i = e.target.parentElement;
     listaT.removeChild(i);
-  }); 
+
+    if (inputC.checked) {
+      contadorT--;
+      letra.innerHTML = contadorT;
+    }
+
+
+    let cargar=document.querySelectorAll("li")
+    console.log(cargar)
+
+    if (!cargar.length!=0) {
+
+
+    
+      mensaje.style.display="block"
+    }
+
+
+
+  });
 
   mensaje.addEventListener("string", function () {
     if (condition) {
-      
     }
-  })
+  });
 
   contenedorTareas.value = "";
 
   inputC.addEventListener("click", function () {
     if (inputC.checked) {
       contadorT = contadorT + 1;
-
       letra.innerHTML = contadorT;
     } else {
       contadorT = contadorT - 1;
       letra.innerHTML = contadorT;
     }
   });
-
-  if (!tarea == "") {
+  
   } else {
     alert("Ingrese una tarea válida");
   }
